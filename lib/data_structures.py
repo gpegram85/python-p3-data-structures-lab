0ad0a7189d1d6f7eb74b009de5a95d3e0c88a608
Grey Pegram <dpegram85@gmail.com>
2024-10-30 22:37:51 -0400

    Completed Lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,42 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    names = [item["name"] for item in spicy_foods]
+    return names
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = [item for item in spicy_foods if item["heat_level"] > 5]
+    return spiciest_foods
 
 def print_spicy_foods(spicy_foods):
-    pass
+    replacement_character = "🌶"
+    for food in spicy_foods:
+        name = food["name"]
+        cuisine = food["cuisine"]
+        heat_level = food["heat_level"]
+        print(f'{name} ({cuisine}) | Heat Level: {heat_level * replacement_character}')
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for item in spicy_foods:
+        if item["cuisine"] == cuisine:
+            return item
+    return None
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    replacement_character = "🌶"
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            name = food["name"]
+            cuisine = food["cuisine"]
+            heat_level = food["heat_level"]
+            print(f'{name} ({cuisine}) | Heat Level: {heat_level * replacement_character}')
 
 def get_average_heat_level(spicy_foods):
-    pass
+    count = 0
+    for food in spicy_foods:
+        count += food["heat_level"]
+    return count / len(spicy_foods)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
